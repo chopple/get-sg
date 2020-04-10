0756781adb8192a4fb95858f2330b38065408b95
chopple <cory.hopple@gmail.com>
2020-04-10 10:38:39 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # get-sg
 Query for Security groups in your AWS environment
+
+
+To use this module copy it to the following directory:
+C:\Program Files\WindowsPowerShell\Modules\get-sg
+you will need to create the get-sg folder
+
+This can lookup by Security Group Name or Name Tag
+
+get-sg sg-123456
+
+get-sg mysecuritygroup
---
+++ get-sg.psm1
@@ -0,0 +1,53 @@
+function Get-SG {
+    [cmdletBinding()]
+
+    param 
+    (
+        [string]$sgdet
+    )
+
+    Try {
+        switch -Regex ($sgdet) {
+            
+            '^(?!sg-)[a-zA-Z]' { $sgfilter = @{name = 'tag:Name'; values = "*" + "$sgdet" + "*" }
+                $SecurityGroupDet = get-ec2securitygroup -filter $sgfilter
+                $properties = [ordered]@{id = $SecurityGroupDet.GroupID
+                    Description             = $SecurityGroupDet.Description
+                    GroupName               = $SecurityGroupDet.GroupName
+                    IPIngress               = $SecurityGroupDet.IPPermissions
+                    IPEgress                = $SecurityGroupDet.IPPermissionsEgress
+                    Name                    = $SecurityGroupDet.tag | Where-Object { $_.key -eq 'Name' } | select-object -ExpandProperty value
+                    Env                     = $SecurityGroupDet.tag | Where-Object { $_.key -eq 'Environment' } | select-object -ExpandProperty value 
+                    VpcId                   = $SecurityGroupDet.vpcid
+                }
+                $obj = New-Object -TypeName psobject -Property $properties 
+            }
+            '^sg-*' {
+                $SecurityGroupDet = get-ec2securitygroup $sgdet
+                $properties = [ordered]@{id = $SecurityGroupDet.GroupID
+                    Description             = $SecurityGroupDet.Description
+                    GroupName               = $SecurityGroupDet.GroupName
+                    IPIngress               = $SecurityGroupDet.IPPermissions
+                    IPEgress                = $SecurityGroupDet.IPPermissionsEgress
+                    Name                    = $SecurityGroupDet.tag | Where-Object { $_.key -eq 'Name' } | select-object -ExpandProperty value 
+                    Env                     = $SecurityGroupDet.tag | Where-Object { $_.key -eq 'Environment' } | select-object -ExpandProperty value 
+                    VpcId                   = $SecurityGroupDet.vpcid
+                }
+                $obj = New-Object -TypeName psobject -Property $properties 
+            }
+            
+        }
+    }
+    Catch { Write-Output "Unknown Input.  Please enter a Host name, an Instnace name or an IP address - wildcards (*) are assumed.  If you entered the correct name the instance may not exist" }
+
+    #write-output $obj
+    if ($obj.id.count -lt 2) {
+    
+        write-output $obj
+    }
+    else {
+        for ($i = 0; $i -lt $obj.id.Count; $i++) {
+            write-output "$($obj.id[$i]) `t $($obj.GroupName[$i]) `t $($obj.Name[$i]) `t $($obj.Env[$i]) `t $($obj.VpcId[$i]) `t $($obj.Description[$i])" 
+        }
+    }
+}
